4538aef0af975a397ce57782e35630819db98c91
sacry- <nitsche25@gmail.com>
2015-05-22 13:10:08 +0200

    initial rails

---
+++ my-awesome-app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAwesomeApp::Application.config.secret_token = 'a50f55d06489b794bb14bd87730392971c1406ad851f991d565093a9b54437aec31a079c72f5d61ee1777005692be2ce8f558b7ca59e10c88d81a616b23f5c6a'
