4124cbea91a90f3486a9944d9ffab617c70dbacf
sacry- <nitsche25@gmail.com>
2015-05-27 08:17:04 +0200

    removed php files

--- isUserActive.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-
-if(strlen($_SESSION["username"]) <= 0  ){
-header ("Location:/team15/index.html"); 
-}
-
-?>
--- routes.php
+++
@@ -1,8 +0,0 @@
-
-<? php
-
-define("BASE", "/var/www/team15/");
-define("ADMIN", "$BASE/admin/index.html");
-define("USER", "$BASE/user/index.html");
-
-?>
